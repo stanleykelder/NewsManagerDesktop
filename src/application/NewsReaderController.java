51f6fb03b9b53c5535ca8a8d38a31c467255de55
Tobias Piffrader <ga54biq@mytum.de>
2018-11-18 23:59:56 +0100

    Category filtering works

--- src/application/NewsReaderController.java
+++ src/application/NewsReaderController.java
@@ -59,80 +59,95 @@ public class NewsReaderController {
 	private ObservableList<String> menuItems;
 
 	@FXML
-    private Label idUserLabel;
-	
-	@FXML
-    private ImageView imgView;
+	private Label idUserLabel;
 
-    @FXML
-    private ListView<String> menuList;
-    
-    @FXML
-    private ListView<Article> headlineList;
-    
-    @FXML
-    private WebView bodyWebView;
-	
 	@FXML
-    private ComboBox<Categories> categoryBox;
-	
-    @FXML
-    private Button readMoreBtn;
-    
-    
-	
+	private ImageView imgView;
+
+	@FXML
+	private ListView<String> menuList;
+
+	@FXML
+	private ListView<Article> headlineList;
+
+	@FXML
+	private WebView bodyWebView;
+
+	@FXML
+	private ComboBox<Categories> categoryBox;
+
+	@FXML
+	private Button readMoreBtn;
 
 	public NewsReaderController() {
-		//Uncomment next sentence to use data from server instead dummy data
+		// Uncomment next sentence to use data from server instead dummy data
 		// newsReaderModel.setDummyData(false);
-		//Get text Label
 		categoryList = newsReaderModel.getCategories();
 		articleList = newsReaderModel.getArticles();
-		menuItems = FXCollections.observableArrayList (
-				"Load news from file", "Login", "New", "Exit", "Edit", "Delte");
-		//menuList.setOrientation(Orientation.HORIZONTAL);
+		menuItems = FXCollections.observableArrayList("Load news from file", "Login", "New", "Exit", "Edit", "Delte");
 		System.out.println(articleList + "constructor");
 	}
 
 	@FXML
-    void initialize() {
-        assert categoryList != null : "fx:id=\"categoryList\" was not injected: check your FXML file 'NewsReader.fxml'.";
-        assert headlineList != null : "fx:id=\"headlineList\" was not injected: check your FXML file 'NewsReader.fxml'.";
-        assert bodyWebView != null : "fx:id=\"bodyWebView\" was not injected: check your FXML file 'NewsReader.fxml'.";
-        getData();
-        this.categoryBox.setItems(categoryList);
-        this.categoryBox.getSelectionModel().selectFirst();
-        System.out.println(this.articleList.toString() + "initialize");
-        this.headlineList.setItems(articleList);
-        this.menuList.setItems(menuItems);
-        ///newsManager18-19Students 2/src/images/abstract1.jpg
-        this.headlineList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Article>() {
-
+	void initialize() {
+		assert categoryList != null : "fx:id=\"categoryList\" was not injected: check your FXML file 'NewsReader.fxml'.";
+		assert headlineList != null : "fx:id=\"headlineList\" was not injected: check your FXML file 'NewsReader.fxml'.";
+		assert bodyWebView != null : "fx:id=\"bodyWebView\" was not injected: check your FXML file 'NewsReader.fxml'.";
+		getData();
+		this.categoryBox.setItems(categoryList);
+		this.categoryBox.getSelectionModel().selectFirst();
+		this.headlineList.setItems(articleList);
+		this.menuList.setItems(menuItems);
+		// for category filter
+		FilteredList<Article> filteredItems = new FilteredList<Article>(articleList, p -> true);
+		this.headlineList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Article>() {
 			@Override
 			public void changed(ObservableValue<? extends Article> observable, Article oldValue, Article newValue) {
 				if (newValue != null) {
-					//btnNext.setDisable(false);
+					readMoreBtn.setDisable(false);
 					bodyWebView.getEngine().loadContent(newValue.getAbstractText());
 					System.out.println(newValue.getImageData());
 					imgView.setImage(newValue.getImageData());
 				} else {
 					bodyWebView.getEngine().loadContent("");
+					readMoreBtn.setDisable(true);
 				}
-				
+
 			}
-        	
-        });
-    }
-	
-	
-	
-	 /*
-     * Open the editing form and pass the contact data. 
-     * It waits until edit is finished and, at the end, update contact data
-     */
-    @FXML
-    void onReadMore(ActionEvent event) {
-    	Scene parentScene = ((Node) event.getSource()).getScene();
+
+		});
+
+		this.categoryBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Categories>() {
+			@Override
+			public void changed(ObservableValue<? extends Categories> observable, Categories oldValue,
+					Categories newValue) {
+				if (newValue != Categories.ALL || newValue == oldValue) {
+					filteredItems.setPredicate(item -> {
+						if (item.getCategory() == newValue.toString()) {
+							return true;
+						} else {
+							return false;
+						}
+					});
+					System.out.println(newValue + " if  ");
+					headlineList.setItems(filteredItems);
+				} else {
+					headlineList.setItems(articleList);
+				}
+
+			}
+
+		});
+
+	}
+
+	/**
+	 * Open the editing form and pass the contact data. It waits until edit is
+	 * finished and, at the end, update contact data
+	 */
+	@FXML
+	void onReadMore(ActionEvent event) {
+		Scene parentScene = ((Node) event.getSource()).getScene();
 		FXMLLoader loader = null;
 		try {
 			loader = new FXMLLoader(getClass().getResource("NewsDetails.fxml"));
@@ -143,32 +158,25 @@ public class NewsReaderController {
 			Stage stage = new Stage();
 			stage.initOwner(parentStage);
 			stage.setScene(scene);
-			//Get the controller for NewsDetailsController.fxml
+			// Get the controller for NewsDetailsController.fxml
 			NewsDetailsController controller = loader.<NewsDetailsController>getController();
 			Article selected = this.headlineList.getSelectionModel().getSelectedItem();
-			//Another way is to pass the contact
 			System.out.println("Article selected: " + selected);
 			controller.setArticle(selected);
-					/**selected.getAbstractText(), selected.getBodyText(),
-					selected.getCategory(), selected.getIdArticle(), selected.getIdImage(), selected.getIdUser(),
-					selected.getImageData(), selected.getPublicationDate(),
-					selected.getSubtitle(), selected.getTitle());*/
-			//Uncomment next sentence if you want and undecorated window 
-			//stage.initStyle(StageStyle.UNDECORATED);
-			//user response is required before continuing with the program
+			// Uncomment next sentence if you want and undecorated window
+			// stage.initStyle(StageStyle.UNDECORATED);
+			// user response is required before continuing with the program
 			stage.initModality(Modality.WINDOW_MODAL);
-			stage.showAndWait();	
-		
+			stage.showAndWait();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
-	
 
 	private void getData() {
-		//TODO retrieve data and update UI
-		//The method newsReaderModel.retrieveData() can be used to retrieve data  
+		// retrieve data and update UI
 		newsReaderModel.retrieveData();
 	}
 
@@ -179,25 +187,26 @@ public class NewsReaderController {
 		return usr;
 	}
 
-	void setConnectionManager (ConnectionManager connection){
-		// this.newsReaderModel.setDummyData(false); //System is connected so dummy data are not needed
+	void setConnectionManager(ConnectionManager connection) {
+		// this.newsReaderModel.setDummyData(false); //System is connected so dummy data
+		// are not needed
 		this.newsReaderModel.setConnectionManager(connection);
 		this.getData();
 	}
-	
+
 	/**
 	 * @param usr the usr to set
 	 */
 	void setUsr(User usr) {
-		
+
 		this.usr = usr;
-		//Reload articles
+		// Reload articles
 		this.getData();
-		//TODO Update UI
+		// TODO Update UI
 	}
 
 	// Auxiliary methods
-	private interface  InitUIData <T>{
-		void initUIData (T loader);
+	private interface InitUIData<T> {
+		void initUIData(T loader);
 	}
 }
